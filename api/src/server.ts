bcbb27e5f42e331a30329797635da28d8011b5fb
pedro-orvalho <pedro.orvalho.com>
2023-09-04 15:10:33 +0200

    setting database

---
+++ api/src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import app from "./app";
+
+dotenv.config();
+
+const port = 4000;
+
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running at port ${port}.`);
+    });
+  })
+  .catch((error: Error) => {
+    console.log(
+      "MongoDB connection error. Please make sure the database is running."
+    );
+    process.exit(1);
+  });
