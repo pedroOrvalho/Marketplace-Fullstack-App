d5bba1757e81edb827b14d19b5daf5dd29893fc3
Ruth Samuel <sapphirecarephysiotherapy@gmail.com>
2023-09-06 12:23:22 +0100

    pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,29 +3,29 @@ import React from 'react';
 import {Routes,Route} from "react-router-dom"
 import './App.css';
 import Navbar from './components/navBar/NavBar';
+import Marketplace from './components/pages/Marketplace';
 import Buying from './components/pages/Buying';
 import Selling from './components/pages/Selling';
 import GoToMarketplace from './components/pages/GoToMarketplace';
+import Footer from './components/pages/Footer';
 
 
 
 function App() {
   return (
-<div>
-  <Navbar/>
+    <div>
+      <Navbar />
 
- <Routes>
-<Route path="/buying" element={<Buying />} />
-<Route path="/selling" element={<Selling />} />
- <Route path="/go-to-marketplace" element={<GoToMarketplace />} />
-  </Routes>
+      <Routes>
+        <Route path="/" element={<Marketplace />} />
+        <Route path="/buying" element={<Buying />} />
+        <Route path="/selling" element={<Selling />} />
+        <Route path="/go-to-marketplace" element={<GoToMarketplace />} />
+      </Routes>
 
-</div>
-
-
-
-  
-  )
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/pages/Marketplace.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import MarketItem from './MarketItem';
+import PublicSharpIcon from "@mui/icons-material/PublicSharp";
+import ChatBubbleOutlineSharpIcon from "@mui/icons-material/ChatBubbleOutlineSharp";
+
+export default function Marketplace() {
+  return (
+    <div>
+      <div>
+        <h1 className="discover">
+          Discover, buy and <br />
+          <span className="goods">sell goods with Marketplace.</span>
+        </h1>
+        <div className="explore">
+          <button className="btn_explore">Explore Marketplace</button>{" "}
+          <span>Create Listing</span> <br />
+          <p className="ecommerce">
+            Marketplace is an e-commerce platform that connects sellers and
+            buyers through meaningful interactions and unique goods.
+          </p>
+        </div>
+      </div>
+      <div className="marketItem">
+        <MarketItem />
+      </div>
+
+      <div>
+        <h1>Why people love Marketplace</h1>
+      </div>
+      <div className="why">
+        <div>
+          <p> fingericon</p>
+          <p>
+            <strong>Free to use</strong>
+          </p>
+          <p>
+            Anyone with an active account can list or buy items with no hidden
+            fees
+          </p>
+
+          <div>
+            <p>
+              <PublicSharpIcon
+                style={{
+                  color: "grey",
+                  width: 400,
+                  height: 100,
+                }}
+              />
+            </p>
+            <p>
+              <strong> Local and nationwide</strong>
+            </p>
+            <p>
+              Browse and buy, or list and sell items to other Facebook users
+              across the country
+            </p>
+          </div>
+          <p>
+            <ChatBubbleOutlineSharpIcon
+              style={{
+                color: "grey",
+                width: 400,
+                height: 100,
+              }}
+            />
+          </p>
+          <p>
+            <strong> Connect with others</strong>
+          </p>
+          <p>
+            Whether you're selling or buying, you can chat with others through
+            Messenger
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
