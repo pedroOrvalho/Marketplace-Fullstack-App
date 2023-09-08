acf97448118feef0595d8264df2a951027eb738a
pedro-orvalho <pedro.orvalho.com>
2023-09-08 05:50:01 +0200

    api user routes/controllers/service added

--- api/src/routers/users.ts
+++ api/src/routers/users.ts
@@ -5,16 +5,32 @@ import {
   createUser,
   logInWithEmail,
   loginWithGoogle,
+  updateUserInfo,
+  deleteUser
 } from "../controllers/users";
 
 const router = Router();
 
 router.post("/register", createUser);
+
 router.post("/login", logInWithEmail);
+
 router.post(
   "/google-login",
   passport.authenticate("google-id-token", { session: false }),
   loginWithGoogle
+);
+
+router.put(
+  "/:userId",
+  passport.authenticate("jwt", { session: false }),
+  updateUserInfo
+);
+
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  deleteUser
 );
 
 export default router;
