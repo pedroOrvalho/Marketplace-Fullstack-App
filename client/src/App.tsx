8ccea2069ce08f0d1b701ee7aaa2c03aa99e1214
pedro-orvalho <pedro.orvalho.com>
2023-09-08 05:20:13 +0200

    simple googleLogIn in client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navBar/NavBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/Registration/login";
 import Buying from "./pages/Buying";
 import Selling from "./pages/Selling";
 import Footer from "./components/Footer";
-import Login from "./pages/Registration/login";
 import Market from "./pages/marketplace/market";
+import GoogleLogIn from "./components/googleLogIn/GoogleLogIn";
 
 import "./App.css";
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/buying" element={<Buying />} />
         <Route path="/selling" element={<Selling />} />
         <Route path="/marketplace" element={<Market />} />
+        <Route path="/google-login" element={<GoogleLogIn />} />
       </Routes>
       <Footer />
     </div>
--- client/src/components/googleLogIn/GoogleLogIn.tsx
+++ client/src/components/googleLogIn/GoogleLogIn.tsx
@@ -1,6 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+
+import { GoogleLogin } from "@react-oauth/google";
+
+export type UserGoogle = {
+  _id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+};
 
 export default function GoogleLogIn() {
+  const [userGoogle, setUserGoogle] = useState<UserGoogle>({
+    _id: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+  });
+  // function
   return (
-    <div>GoogleLogIn</div>
-  )
+    <div>
+      <h1>GoogleLogIn</h1>
+      <GoogleLogin
+        // type="icon"
+        size="medium"
+        shape="rectangular"
+        onSuccess={async (credentialResponse) => {
+          console.log(credentialResponse, "credential");
+          const url = "http://localhost:7000/user/google-login";
+         
+          // we save the credential in a variable
+          const credential = credentialResponse.credential;
+         
+          //we send from the frontend to the backend using post methot to the endpoint
+          //we defined in the backend. 
+          let res = await axios.post(url, { id_token: credential });
+     
+          // if the comparison from the credential we send from the frontend with
+          // the credential with google is correct we receive the user information
+          // and save it to setUserGoogle state.
+          if (res.status === 200) {
+            console.log(res, "response from BE");
+            setUserGoogle(res.data.userData);
+          } else {
+            alert("Login false");
+          }
+    
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+      <h1> user information from google</h1>
+      <p> first Name:{userGoogle.firstName} </p>
+      <p> last name: {userGoogle.lastName}</p>
+    </div>
+  );
 }
