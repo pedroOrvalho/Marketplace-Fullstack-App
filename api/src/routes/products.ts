f6165c7d234ea62a99930fc06be3dfbb4af3d331
pedro-orvalho <pedro.orvalho.com>
2023-09-08 06:11:36 +0200

    api product logic for routes/controllers/services

---
+++ api/src/routes/products.ts
@@ -0,0 +1,42 @@
+import Router from "express";
+import passport from "passport";
+
+import {
+  CreateAllProducts,
+  getAllProductByUserId,
+  getAllProducts,
+  getProductDetails,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/products";
+
+const router = Router();
+
+router.post(
+  "/:_id",
+  passport.authenticate("jwt", { session: false }),
+  CreateAllProducts
+);
+
+router.get(
+  "/:userId",
+  passport.authenticate("jwt", { session: false }),
+  getAllProductByUserId
+);
+
+router.get("/:productId", getProductDetails);
+router.get("/", getAllProducts);
+
+router.put(
+  "/:productId",
+  passport.authenticate("jwt", { session: false }),
+  updateProduct
+);
+
+router.delete(
+  "/:productId",
+  passport.authenticate("jwt", { session: false }),
+  deleteProduct
+);
+
+export default router;
