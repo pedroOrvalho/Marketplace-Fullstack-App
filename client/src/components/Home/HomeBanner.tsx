ebdebce92acfcee76ebd77af1e19c1759a4ccdcb
pedro-orvalho <pedro.orvalho.com>
2023-09-10 03:44:14 +0200

    hero banner added with styling

---
+++ client/src/components/Home/HomeBanner.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+import { Box } from "@mui/material";
+
+export default function HomeBanner() {
+  return (
+    <Box className="home_banner_container">
+      <Box className="home_banner_text_container">
+        <h1>
+          Discover, buy and sell <br />
+          goods with Marketplace.
+        </h1>
+        <h2>
+          Marketplace is an e-commerce platform that connects sellers and buyers
+          through meaningful interactions and unique goods.
+        </h2>
+        <Link to="/marketplace">
+          <button className="btn_explore">Explore Marketplace</button>
+        </Link>
+      </Box>
+    </Box>
+  );
+}
+
