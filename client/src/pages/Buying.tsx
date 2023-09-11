ae3abf6384fb8618297fa9c9655ea7c21ea13197
pedro-orvalho <pedro.orvalho.com>
2023-09-11 06:17:35 +0200

    buying page responsive styling added

--- client/src/pages/Buying.tsx
+++ client/src/pages/Buying.tsx
@@ -1,4 +1,3 @@
-import shopper from "../images/shopper.jpg";
 import BuyingBanner from "../components/buying/BuyingBanner";
 import BuyingIcons from "../components/buying/BuyingIcons";
 
@@ -6,34 +5,7 @@ export default function Buying() {
   return (
     <div className="buyingPage_container">
       <BuyingBanner />
-      <BuyingIcons />
+      <BuyingIcons />  
     </div>
   );
 }
-
-/*
-
-
-      <div className="image">
-        <div className="image1">
-          <img src={shopper} alt="" style={{ width: 400 }} />
-        </div>
-      </div>
-      
-      <div className="buying_page1">
-        <p className="buyingpage">
-          <HouseIcon
-            style={{
-              color: "white",
-              width: 400,
-              height: 100,
-            }}
-          />
-        </p>
-        <p className="buy_sell">Buy or Sell on Marketplace</p>
-        <Link to="/">
-          <button className="btn3">Go to Marketplace</button>
-        </Link>
-      </div>
-
-*/
