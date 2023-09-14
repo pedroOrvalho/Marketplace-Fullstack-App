0bfe9c4d38db230dfcf55a11a59b30273ffbc475
pedro-orvalho <pedro.orvalho.com>
2023-09-15 01:13:02 +0200

    productList with responsiveness

--- client/src/pages/marketplace/ProductItem.tsx
+++ client/src/pages/marketplace/ProductItem.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import { Product } from "../../components/types/type";
+import { Link } from "react-router-dom";
 
 type Prop = {
   item: Product;
@@ -7,42 +8,40 @@ type Prop = {
 
 export default function ProductItem({ item }: Prop) {
   return (
-    <Box
-      sx={{
-        padding: 2,
-        backgroundColor:"white"
-      }}
-    >
+    <Link to={`/${item._id}`}>
       <Box
         sx={{
-          width: "100%",
-          position: "relative",
-          paddingBottom: "100%",
-          backgroundColor: "gray",
-          borderRadius: "20px",
+          padding: 2,
+          backgroundColor: "white",
         }}
       >
-        <img
-          src={item.image[0]}
-          alt={item.title}
-          style={{
-            borderRadius: "20px",
-            position: "absolute",
-            top: "0",
-            left: "0",
+        <Box
+          sx={{
             width: "100%",
-            height: "100%",
+            position: "relative",
+            paddingBottom: "100%",
+            backgroundColor: "gray",
+            borderRadius: "20px",
           }}
-        />
+        >
+          <img
+            src={item.image[0]}
+            alt={item.title}
+            style={{
+              borderRadius: "20px",
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </Box>
+        <Box className="productItem_text_container">
+          <p className="productItem_text">{item.price} kr</p>
+          <p className="productItem_text">{item.title}</p>
+        </Box>
       </Box>
-      <Box>
-        <p>{item.price} kr</p>
-        <p>{item.title}</p>
-      </Box>
-    </Box>
+    </Link>
   );
 }
-
-/*
-
-*/
