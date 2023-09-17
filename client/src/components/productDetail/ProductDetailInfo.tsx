8183a9fd4e0964d67fcb8c522b5294d11bfdd2d3
pedro-orvalho <pedro.orvalho.com>
2023-09-17 02:38:21 +0200

    Product detail slider added and basic folder structure

---
+++ client/src/components/productDetail/ProductDetailInfo.tsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+
+export default function ProductDetailInfo() {
+  const productDetail = useSelector(
+    (state: RootState) => state.products.productDetail
+  );
+
+  return (
+    <div className="productDetail_info_container">
+      <h1>{productDetail?.title}</h1>
+    </div>
+  );
+}
--- client/src/pages/ProductDetail.tsx
+++ client/src/pages/ProductDetail.tsx
@@ -1,19 +1,15 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import FsLightbox from "fslightbox-react";
 
-import { AppDispatch, RootState } from "../redux/store";
+import { AppDispatch } from "../redux/store";
 import { fetchProductById } from "../redux/thunk/product";
+import ProductDetailSlider from "../components/productDetail/ProductDetailSlider";
+import ProductDetailInfo from "../components/productDetail/ProductDetailInfo";
 
 export default function ProductDetail() {
-  const productDetail = useSelector(
-    (state: RootState) => state.products.productDetail
-  );
-  console.log(productDetail, "PRODUCT DETAIL INFO");
   const dispatchThunk = useDispatch<AppDispatch>();
   const { id } = useParams();
-  const [toggler, setToggler] = useState(false);
 
   useEffect(() => {
     dispatchThunk(fetchProductById(id));
@@ -21,15 +17,8 @@ export default function ProductDetail() {
 
   return (
     <div className="productDetail_container">
-      <div className="productDetail_slider_container">
-        <button onClick={() => setToggler(!toggler)}>
-          <img src={productDetail?.image[1]} alt={productDetail?.title} />
-        </button>
-        <FsLightbox toggler={toggler} sources={productDetail?.image} />
-      </div>
-      <div className="productDetail_info_container">
-        <h1>{productDetail?.title}</h1>
-      </div>
+      <ProductDetailSlider />
+      <ProductDetailInfo />
     </div>
   );
 }
